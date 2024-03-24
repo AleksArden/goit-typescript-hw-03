fc158bdc3efd0fbeba819377b938e13baf1aaf73
Oleksii Chubenko <i1723@i.ua>
2024-03-24 14:13:55 +0000

    Solution of the task 4 was added

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,45 @@
+class Key {
+  private signature: number = Math.round(Math.random() * (10 - 1) + 1);
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): number {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+
+  public tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: number): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  public openDoor(key: number): void {
+    if (key === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
+const key1 = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
